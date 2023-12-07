1b4d34dcbd8d01b8e825e2cd0601a77036ad0303
Harkavik Volha <volha_harkavik@senla.eu>
2023-12-07 15:24:55 +0300

    update first test for saucedemo.com

--- src/test/java/FirstTest.java
+++ src/test/java/FirstTest.java
@@ -2,13 +2,15 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import java.util.concurrent.TimeUnit;
+
 
 
 public class FirstTest {
     public static void main(String[] args) throws InterruptedException {
         System.setProperty("webdriver.chrome.driver","C:\\programs\\WebDriver\\chromedriver-win64\\chromedriver.exe");
         WebDriver driver = new ChromeDriver();
-//        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+//        driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
 
         driver.get("https://www.saucedemo.com/");
         driver.findElement(By.id("user-name")).sendKeys("standard_user");
