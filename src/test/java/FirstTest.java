0570dc434709f0984f984804dfafc9a4bd6422d7
Harkavik Volha <volha_harkavik@senla.eu>
2023-12-07 14:59:48 +0300

    update first test for saucedemo.com

--- src/test/java/FirstTest.java
+++ src/test/java/FirstTest.java
@@ -3,9 +3,6 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-import java.time.Duration;
-import java.util.List;
-import java.util.Set;
 
 public class FirstTest {
     public static void main(String[] args) throws InterruptedException {
@@ -15,12 +12,17 @@ public class FirstTest {
 
         driver.get("https://www.saucedemo.com/");
         driver.findElement(By.id("user-name")).sendKeys("standard_user");
-//        Thread.sleep(5000);
+        Thread.sleep(3000);
         driver.findElement(By.id("password")).sendKeys("secret_sauce");
-//        Thread.sleep(5000);
+        Thread.sleep(3000);
         driver.findElement(By.cssSelector("input#login-button")).click();
-//        driver.findElement(By.xpath("//a[@id=\"item_4_title_link\"]")).click();
-//        driver.findElement(By.cssSelector("button#add-to-cart-sauce-labs-backpack")).click();
+        Thread.sleep(3000);
+        driver.findElement(By.xpath("//a[@id=\"item_4_title_link\"]")).click();
+        Thread.sleep(3000);
+        driver.findElement(By.cssSelector("button#add-to-cart-sauce-labs-backpack")).click();
+        Thread.sleep(3000);
+        driver.findElement(By.id("back-to-products")).click();
+        Thread.sleep(3000);
         WebElement element = driver.findElement(By.xpath("//div[@class='inventory_item_name '][1]"));
         String elementname = element.findElement(By.xpath("//div[@class='inventory_item_name ']")).getText();
         System.out.println(elementname);
