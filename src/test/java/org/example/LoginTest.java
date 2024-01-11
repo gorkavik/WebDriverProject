ff13f7be95a0a97aba855f546d0839af57aad28e
Harkavik Volha <volha_harkavik@senla.eu>
2024-01-11 15:14:40 +0300

    test for saucedemo.com using pageobject

---
+++ src/test/java/org/example/LoginTest.java
@@ -0,0 +1,59 @@
+package org.example;
+
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.concurrent.TimeUnit;
+
+public class LoginTest {
+    public static LoginPage loginPage;
+    //public static ProfilePage profilePage;
+    public static WebDriver driver;
+
+    /**
+     * осуществление первоначальной настройки
+     */
+    @BeforeClass
+    public static void setup() {
+        //определение пути до драйвера и его настройка
+        System.setProperty("webdriver.chrome.driver", ConfProperties.getProperty("chromedriver"));
+        //создание экземпляра драйвера
+        driver = new ChromeDriver();
+        loginPage = new LoginPage(driver);
+        //profilePage = new ProfilePage(driver);
+        //окно разворачивается на полный экран
+        driver.manage().window().maximize();
+        //задержка на выполнение теста = 10 сек.
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        //получение ссылки на страницу входа из файла настроек
+        driver.get(ConfProperties.getProperty("loginpage"));
+    }
+
+    @Test
+    public void loginTest() {
+        //значение login/password берутся из файла настроек по аналогии с chromedriver и loginpage
+
+        //вводим логин
+        loginPage.inputLogin(ConfProperties.getProperty("login"));
+        //вводим пароль
+        loginPage.inputPasswd(ConfProperties.getProperty("password"));
+        //нажимаем кнопку входа
+        loginPage.clickLoginBtn();
+
+        //получаем отображаемый логин
+//        String user = profilePage.getUserName();
+//        //и сравниваем его с логином из файла настроек
+//        Assert.assertEquals(ConfProperties.getProperty("login"), user);
+    }
+
+    @AfterClass
+    public static void tearDown() throws InterruptedException{
+//        profilePage.entryMenu();
+//        profilePage.userLogout();
+        Thread.sleep(3000);
+        driver.close();
+        driver.quit(); }
+}
